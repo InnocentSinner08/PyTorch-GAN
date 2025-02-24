5b25f10f36e6944ea5428764a81b7f3ff26c655f
InnocentSinner08 <innocentsinner08114@gmail.com>
2025-02-24 13:10:29 +0530

    doe

--- SNGAN/main.py
+++ SNGAN/main.py
@@ -17,7 +17,6 @@ import warnings
 warnings.filterwarnings("ignore")
 
 def save_generated_images(netG, epoch, device, noise_size=128, num_images=16):
-    """Generate and save images from the generator."""
     with torch.no_grad():
         fixed_noise = torch.randn(num_images, noise_size, device=device)
         fake_images = netG(fixed_noise).cpu()
@@ -35,8 +34,7 @@ def main():
                            ]), download=True, train=True)
 
     subset = torch.utils.data.Subset(dataset, np.arange(int(len(dataset) * args.data_ratio)))
-    dataloader = torch.utils.data.DataLoader(subset, batch_size=args.batch_size,
-                                             shuffle=True, num_workers=args.workers)
+    dataloader = torch.utils.data.DataLoader(subset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
 
     netD = ResDiscriminator32().to(device)
     netG = Regan_training(ResGenerator32(args.noise_size).to(device), sparsity=args.sparsity)
@@ -46,81 +44,58 @@ def main():
 
     print("Starting Training Loop...")
 
-    flag_g = 1
-    fid_scores = []
+    prev_loss = float('inf')
+    loss_plateau_counter = 0
+    patience = 5  # Number of epochs to wait before updating masks
+    loss_threshold = 0.01  # Minimum loss change to consider improvement
 
     for epoch in range(1, args.epoch + 1):
-
-        if args.regan:
-            if epoch < args.warmup_epoch + 1:
-                print('Warmup training...')
-                netG.train_on_sparse = False
-            elif epoch > args.warmup_epoch and flag_g < args.g + 1:
-                print(f'Epoch {epoch}, Sparse training...')
-                netG.turn_training_mode(mode='sparse')
-                if flag_g == 1:
-                    for params in optimizerG.param_groups:
-                        params['lr'] = args.lr
-                flag_g += 1
-            elif epoch > args.warmup_epoch and flag_g < 2 * args.g + 1:
-                print(f'Epoch {epoch}, Dense training...')
-                netG.turn_training_mode(mode='dense')
-                if flag_g == args.g + 1:
-                    for params in optimizerG.param_groups:
-                        params['lr'] = args.lr * 0.1
-                flag_g += 1
-                if flag_g == 2 * args.g + 1:
-                    flag_g = 1
+        running_loss_G = 0.0
 
         for i, data in enumerate(dataloader, 0):
-
             netD.zero_grad()
             real_cpu = data[0].to(device)
             b_size = real_cpu.size(0)
             output = netD(real_cpu).view(-1)
             errD_real = torch.mean(nn.ReLU(inplace=True)(1.0 - output))
             errD_real.backward()
-            D_x = output.mean().item()
-
+            
             noise = torch.randn(b_size, args.noise_size, device=device)
             fake = netG(noise)
-
             output = netD(fake.detach()).view(-1)
             errD_fake = torch.mean(nn.ReLU(inplace=True)(1 + output))
             errD_fake.backward()
-            D_G_z1 = output.mean().item()
-            errD = errD_real + errD_fake
             optimizerD.step()
 
             if i % args.n_critic == 0:
                 netG.zero_grad()
-                noise = torch.randn(b_size, args.noise_size, device=device)
                 fake = netG(noise)
                 output = netD(fake).view(-1)
                 errG = -torch.mean(output)
                 errG.backward()
-                D_G_z2 = output.mean().item()
-
-                if args.regan and netG.train_on_sparse:
-                    netG.apply_masks()
-
                 optimizerG.step()
+                running_loss_G += errG.item()
 
-            if i % 50 == 0:
-                print('[%4d/%4d][%3d/%3d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                      % (epoch, args.epoch, i, len(dataloader),
-                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+        avg_loss_G = running_loss_G / len(dataloader)
+        print(f'Epoch [{epoch}/{args.epoch}] - Generator Loss: {avg_loss_G:.4f}')
 
-        # Save generated images every 10 epochs
+        if args.regan:
+            if abs(prev_loss - avg_loss_G) < loss_threshold:
+                loss_plateau_counter += 1
+            else:
+                loss_plateau_counter = 0
+            
+            if loss_plateau_counter >= patience:
+                print("Loss plateau detected! Updating masks...")
+                netG.update_masks()
+                loss_plateau_counter = 0
+
+            prev_loss = avg_loss_G
+
         if epoch % 10 == 0:
             save_generated_images(netG, epoch, device, args.noise_size)
 
-        # Compute FID score every 10 epochs
-
-    # Save FID scores to file
-
 if __name__ == '__main__':
-    model_name = 'SNGAN'
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--epoch', type=int, default=20)
     argparser.add_argument('--batch_size', type=int, default=64)
@@ -129,18 +104,11 @@ if __name__ == '__main__':
     argparser.add_argument('--image_size', type=int, default=32)
     argparser.add_argument('--noise_size', type=int, default=128)
     argparser.add_argument('--dataroot', type=str, default='../dataset')
-    argparser.add_argument('--clip_value', type=float, default=0.01)
     argparser.add_argument('--n_critic', type=int, default=5)
     argparser.add_argument('--sparsity', type=float, default=0.3)
-    argparser.add_argument('--g', type=int, default=5)
-    argparser.add_argument('--warmup_epoch', type=int, default=100)
     argparser.add_argument('--data_ratio', type=float, default=1.0)
     argparser.add_argument('--regan', action="store_true")
     args = argparser.parse_args()
 
-    if not os.path.exists(args.dataroot):
-        os.makedirs(args.dataroot)
-
     device = "cuda" if torch.cuda.is_available() else "cpu"
-
     main()
